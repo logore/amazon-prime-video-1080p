8f888ac15f2a3bcbd90de0af73ea1d00321bfef7
Sho Maruyama <hell.oh.twitter@gmail.com>
2022-11-14 00:12:41 +0900

    change the code a bit and add new url matches in manifest.json

--- js/content_script.js
+++ js/content_script.js
@@ -1,13 +1,14 @@
-const injectScript = (fn) => {
-  if(typeof fn !== 'function') {
-    return;
+const injectScript = fn => {
+  if (typeof fn !== 'function') {
+    return
   }
-  let match = fn.toString().match(/{.*}/sm);
-  let fnStr = match[0].slice(1, match[0].length - 1);
 
-  document.documentElement.setAttribute('onreset', fnStr);
-  document.documentElement.dispatchEvent(new CustomEvent('reset'));
-  document.documentElement.removeAttribute('onreset');
+  let match = fn.toString().match(/{.*}/sm)
+  let fnStr = match[0].slice(1, match[0].length - 1)
+
+  document.documentElement.setAttribute('onreset', fnStr)
+  document.documentElement.dispatchEvent(new CustomEvent('reset'))
+  document.documentElement.removeAttribute('onreset')
 }
 
 const main = () => {
@@ -16,7 +17,7 @@ const main = () => {
       const sleep = ms => new Promise(r => setTimeout(r, ms))
 
       for(;;) {
-        if(document.documentElement !== null && document.body !== null) {
+        if (document.documentElement !== null && document.body !== null) {
           break
         }
         await sleep(100)
@@ -25,14 +26,14 @@ const main = () => {
       window.__defineGetter__('innerWidth', () => 1920)
       window.__defineGetter__('innerHeight', () => 1080)
       document.documentElement.__defineGetter__('clientWidth', () => 1920)
-      document.documentElement.__defineGetter__('clientHeight', ()=> 1080)
+      document.documentElement.__defineGetter__('clientHeight', () => 1080)
   
       const div = document.createElement('div')
       div.classList.add('anim-box')
       div.classList.add('popup')
 
       document.body.appendChild(div)
-    })();
+    })()
   })
 }
 
